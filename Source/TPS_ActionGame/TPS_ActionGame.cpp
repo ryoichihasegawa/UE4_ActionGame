4ddeb1c97b770fec401789e8528544c34e58ccb0
Mikan-01417 <103237738+Mikan-01417@users.noreply.github.com>
2022-06-24 04:05:51 +0900

    なんかまぁ色々

---
+++ Source/TPS_ActionGame/TPS_ActionGame.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TPS_ActionGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TPS_ActionGame, "TPS_ActionGame" );
